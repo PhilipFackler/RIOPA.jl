7ccabff05bd14159a81faf1aafcaf5a63e5b2a62
Philip Fackler <facklerpw@ornl.gov>
2022-12-05 14:16:58 -0500

    Switch to MPIPreferences method

--- scripts/summit.sh
+++ scripts/summit.sh
@@ -5,8 +5,8 @@ module load hdf5
 module load julia
 
 export JULIA_HDF5_PATH="$OLCF_HDF5_ROOT/bin"
-export JULIA_MPI_PATH=$OLCF_SPECTRUM_MPI_ROOT
-export JULIA_MPI_BINARY=system
 
+julia --project -e 'using Pkg; Pkg.add("MPIPreferences")'
+julia --project -e 'using MPIPreferences; use_system_binary(; mpiexec="jsrun")'
 julia --project -e 'using Pkg; Pkg.build(verbose=true)'
 

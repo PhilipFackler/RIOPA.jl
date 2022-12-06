94c31c32dba8ba48bc05f3f76dad37df14991523
Philip Fackler <facklerpw@ornl.gov>
2022-12-06 16:22:55 -0500

    Added LocalPreferences.toml file for summit

--- scripts/summit.sh
+++ scripts/summit.sh
@@ -6,7 +6,8 @@ module load julia
 
 export JULIA_HDF5_PATH="$OLCF_HDF5_ROOT/bin"
 
-julia --project -e 'using Pkg; Pkg.add("MPIPreferences")'
-julia --project -e 'using MPIPreferences; use_system_binary(; mpiexec="jsrun")'
+readonly self_path=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+cp ${self_path}/SummitPreferences.toml $PWD/LocalPreferences.toml
+
 julia --project -e 'using Pkg; Pkg.build(verbose=true)'
 
